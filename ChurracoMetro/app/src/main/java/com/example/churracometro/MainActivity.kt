4218e7ee28ad73ad977a28dae974fdd07e17396c
LAPTOP-ISJLSABS\user <igor.issao@grupointegrado.br>
2019-04-12 19:30:56 -0300

    0.3

--- ChurracoMetro/app/src/main/java/com/example/churracometro/MainActivity.kt
+++ ChurracoMetro/app/src/main/java/com/example/churracometro/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.example.churracometro
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_calc.setOnClickListener {
+
+            val intent = Intent(applicationContext, ResultadoActivity::class.java)
+
+            val qt_Man = text_man.text.toString()
+            val qt_Women = text_women.text.toString()
+            val qt_Kid = text_kid.text.toString()
+
+            intent.putExtra("man", qt_Man)
+            intent.putExtra("women", qt_Women)
+            intent.putExtra("kid", qt_Kid)
+
+            val validaCampo = validaCampo(qt_Man, qt_Women, qt_Kid)
+
+            if(validaCampo == true){
+                startActivity(intent)
+            }else{
+                text_erro.setText("Preencha todos os campos campos primeiro")
+            }
+        }
+    }
+
+    fun validaCampo(qt_man: String, qt_women: String, qt_kid: String) : Boolean{
+        var camposValidos:Boolean = true
+
+        if(qt_man == null || qt_man.equals("") && qt_women == null || qt_women.equals("") && qt_kid == null || qt_kid.equals("")){
+
+            camposValidos = false
+
+        }
+
+        return camposValidos
     }
 }
--- ChurracoMetro/app/src/main/java/com/example/churracometro/ResultadoActivity.kt
+++ ChurracoMetro/app/src/main/java/com/example/churracometro/ResultadoActivity.kt
@@ -2,11 +2,59 @@ package com.example.churracometro
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_resultado.*
 
 class ResultadoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        var dados: Bundle = intent.extras
+        var qt_man = dados.getString("man")
+        var qt_women = dados.getString("women")
+        var qt_kid = dados.getString("kid")
+
+        calcular(qt_man.toInt(), qt_women.toInt(), qt_kid.toInt())
+
+
+        button.setOnClickListener {
+            finish()
+        }
     }
+
+
+
+    fun calcular(man: Int, women: Int, kid: Int) {
+
+        textPicanhaMan.setText((man * 240).toString())
+        textPicanhaWomen.setText((women * 180).toString())
+        textPicanhaKid.setText((kid * 120).toString())
+        textMaminhaMan.setText((man * 80).toString())
+        textMaminhaWomen.setText((women * 60).toString())
+        textMaminhaKid.setText((kid * 40).toString())
+        textLinguiçaMan.setText((man * 40).toString())
+        textLinguiçaWomen.setText((women * 30).toString())
+        textLinguiçaKid.setText((kid * 20).toString())
+        textFrangoMan.setText((man * 40).toString())
+        textFrangoWomen.setText((women * 30).toString())
+        textFrangoKid.setText((kid * 20).toString())
+        textArrozMan.setText((man * 100).toString())
+        textArrozWomen.setText((women * 80).toString())
+        textArrozKid.setText((0).toString())
+        textBatataMan.setText((man * 200).toString())
+        textBatataWomen.setText((women * 150).toString())
+        textBatataKid.setText((kid * 100).toString())
+        textFarofaMan.setText((man * 100).toString())
+        textFarofaWomen.setText((women * 80).toString())
+        textFarofaKid.setText((0).toString())
+        textPãesMan.setText((man * 1.50).toString())
+        textPãesWomen.setText((women * 1.50).toString())
+        textPãesKid.setText((0).toString())
+
+
+
+
+    }
+
 }
